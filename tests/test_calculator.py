3f1942c783a249b05dbc3f44da4dccf7e986cd78
pgomezb2 <pgomezb2@unemi.edu.ec>
2025-09-27 10:33:28 -0500

    primer envio de pruebas

---
+++ tests/test_calculator.py
@@ -0,0 +1,21 @@
+import unittest
+from src.calculator import suma, resta
+
+class TestCalculator(unittest.TestCase):
+    def test_suma(self):
+        self.assertEqual(suma(2,4), 6)
+        
+    def test_resta(self):
+        self.assertEqual(resta(6,4), 2)
+        
+if __name__ == '__main__':
+    unittest.main()
+
+# from src.calculator import suma, resta
+
+# def test_suma():
+#     assert suma(2,4) == 6
+    
+# def test_resta():
+#     assert resta(6,4) == 2
+        
